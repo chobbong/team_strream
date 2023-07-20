a88770099d583fc8670ada7bed5718ae0669657a
조윤서 <124692313+chobbong@users.noreply.github.com>
2023-07-21 01:11:59 +0900

    Add files via upload

---
+++ app.py
@@ -0,0 +1,18 @@
+import streamlit as st
+
+st.set_page_config(
+    page_icon=":goose:",
+    page_title="부지런한 거위",
+    layout="wide",
+)
+st.write("""
+# 😎 부지런한 거위팀의 프로젝트
+""")
+
+st.write("""
+ 팀장 : 박경훈,
+""")
+
+st.write("""
+ 팀원 : 강규욱, 배나연, 조윤서
+""")
